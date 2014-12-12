8b43b3d4a82e2b956594af71595fae124ebc2e63
djpasica <djpasica92@gmail.com>
2014-12-12 13:59:38 +0200

    basic project init

---
+++ index.py
@@ -0,0 +1,46 @@
+# coding: utf-8 -*-
+
+from twisted.internet import reactor
+from twisted.internet import threads
+from twisted.internet import defer
+
+
+def main_callback(result):
+    #sort by order_priority
+    sorted_res = sorted(result, key=lambda x: x[1][1])
+
+    print ('result:', [item[1][0] for item in sorted_res])
+
+
+def worker(num, order_priority=0):
+    '''
+    Run some number work
+
+    :param num: number to processing
+    :param order_priority: sort priority in result of main callback
+    :return: tuple: (number result, sort priority)
+    '''
+    return num ** 2, order_priority
+
+
+def worker_err(error):
+    print(error)
+    reactor.callFromThread(reactor.stop)
+
+
+def processing(nums):
+    workers_list = []
+    for num, order_priority in nums:
+        workers_list.append(threads.deferToThread(worker, num, order_priority).addErrback(worker_err))
+
+    return defer.DeferredList(workers_list)
+
+
+#numbers to sqrt
+#(x, y): x - number, y - priority
+nums = [(2, 4),(3, 3), (4, 2), (5, 6)]
+
+d = processing(nums)
+d.addCallback(main_callback)
+
+reactor.run()
